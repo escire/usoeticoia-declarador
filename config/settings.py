1a7e7ac3c0edece1953d2255c8e515e3181e6d2d
GustavoCasiano <gustavocasiano@escire.lat>
2026-01-22 21:21:22 -0600

    fix: Correcion de SITE DOMAIN

--- config/settings.py
+++ config/settings.py
@@ -79,6 +79,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'core.context_processors.recaptcha_keys',  # reCAPTCHA keys
+                'core.context_processors.site_settings',  # Site domain and base URL
             ],
         },
     },
